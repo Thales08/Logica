6fc6475a6c77f1ca7f877f48926d9e80669f0943
Thales08 <43183832+Thales08@users.noreply.github.com>
2019-04-29 16:11:39 -0300

    Update site.js

--- switch/site.js
+++ switch/site.js
@@ -8,37 +8,53 @@ function abrirSite() {
     Entrada = parseInt(frmAbrir.txtEntrada.value)
     switch (Entrada) {
         case 1:
-            window.location = "/C:/Users/arnaldo.vyalves/Documents/Logica-arnaldo/logica/caculoIMC/index.html";
+            window.location = "/C:/Users/thales.dlima/Desktop/Logica/logica/CalculoIMC/index.html";
             break
     }
     switch (Entrada) {
         case 2:
-            window.location = "/C:/Users/arnaldo.vyalves/Documents/Logica-arnaldo/logica/celsiousparafrenheit/index.html";
+            window.location = "/C:/Users/thales.dlima/Desktop/Logica/logica/Calculodamedia/index.html";
             break
     }
     switch (Entrada) {
         case 3:
-            window.location = "/C:/Users/arnaldo.vyalves/Documents/Logica-arnaldo/logica/Churrascoladora/index.html";
+            window.location = "/C:/Users/thales.dlima/Desktop/Logica/logica/churrasco/index.html";
             break
-    }
+    } 
     switch (Entrada) {
         case 4:
-            window.location = "/C:/Users/arnaldo.vyalves/Documents/Logica-arnaldo/logica/ConverterDolaremReal/index.html";
+            window.location = "/C:/Users/thales.dlima/Desktop/Logica/logica/celsius/index.html";
             break
     }
     switch (Entrada) {
         case 5:
-            window.location = "/C:/Users/arnaldo.vyalves/Documents/Logica-arnaldo/logica/jogoDado/index.html";
+            window.location = "/C:/Users/thales.dlima/Desktop/Logica/logica/jogoDado/index.html";
             break
     }
     switch (Entrada) {
         case 6:
-            window.location = "/C:/Users/arnaldo.vyalves/Documents/Logica-arnaldo/logica/precoDaHora/index.html";
+            window.location = "/C:/Users/thales.dlima/Desktop/Logica/logica/jokenpo/index.html";
             break
     }
     switch (Entrada) {
         case 7:
-            window.location = "/C:/Users/arnaldo.vyalves/Documents/Logica-arnaldo/logica/raio/index.html";
+            window.location = "/C:/Users/thales.dlima/Desktop/Logica/logica/raio/index.html";
             break
     }
+    switch (Entrada) {
+        case 8:
+            window.location = "/C:/Users/thales.dlima/Desktop/Logica/logica/real/index.html";
+            break
+    } 
+    switch (Entrada) {
+        case 9:
+            window.location = "/C:/Users/thales.dlima/Desktop/Logica/logica/salario/index.html";
+            break
+    }
+    switch (Entrada) {
+        case 10:
+            window.location = "/C:/Users/thales.dlima/Desktop/Logica/logica/Tabuada/index.html";
+            break
+    }      
+
 }
